37f6cf02e7696f4b3c1e4dff4e245e92ebb6b40c
mottybar <mottybar20@gmail.com>
2025-10-30 12:26:41 +0200

    exercise1,2,3

--- materials/vagrant/fixScripts/exercise2-fix.sh
+++ materials/vagrant/fixScripts/exercise2-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise2 here
+
+sudo nano /etc/hosts
+comment out #127.0.0.1 www.ascii-art.de
--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise3 here
+
+sudo nano /etc/apache2/sites-enabled/000-default.conf
+
+<Location "/">
+    Require all granted
+</Location>
